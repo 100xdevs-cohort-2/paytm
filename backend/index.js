7ab50c843f1a92a387ad2a9b2814de2686ad1aaf
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-21 20:22:18 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+const cors = require("cors")
+const mainRouter = require("./routes/index")
 
+app.use(cors());
+app.use(express.json())
+const app = express();
 
+app.use("/api/v1", mainRouter)
+
+app.listen(3016)
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const userRouter = require("./user")
+
+const router = express.Router()
+
+router.use('/user', userRouter)
+
+module.exports = router
+
