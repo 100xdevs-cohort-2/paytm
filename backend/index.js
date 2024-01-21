b1fb3a62797aafad6eaf4ae6d033d9f636effe84
Naman Sharma <namansharma@Namans-MacBook-Air.local>
2024-01-21 23:15:22 +0530

    First commit : backend is read => naman933

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const rootRouter = require("./routes/index");
+const cors = require("cors");
+const port = 8000;
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/v1", rootRouter);
+
+app.listen(port, ()=>{
+    console.log("App is running on the port : " + port);
+});
 
---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const userRouter = require("./user");
+const accountRouter = require("./account");
+const router=express.Router();
+
+
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
+
+module.exports = router;
+
